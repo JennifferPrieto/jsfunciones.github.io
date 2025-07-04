ebe8336a91a5de507a159a31582cc08a77ad261e
Vivian Prieto Espinoza <paolajeny567@gmail.com>
2025-07-04 15:02:39 -0400

    first commit

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,10 @@
+
+const example = function(a, b, c) {
+    return a + b + c;
+};
+
+
+// prueba 
+
+const resultado = example(2, 3, 4);
+console.log("el resultado es:", resultado);
---
+++ assets/js/2_arrow.js
@@ -0,0 +1,8 @@
+const suma = (a, b) => a + b;
+
+
+// prueba
+
+const resultado = suma(5, 7);
+console.log("el resultado es:", resultado);
+
